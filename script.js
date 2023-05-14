eb61bde6c41d2cf7bd511f4a5911b8151314cacf
Lucas Luna <lukaslunasantos_13@hotmail.com>
2023-05-14 14:58:11 -0300

    carrosel criado

--- script.js
+++ script.js
@@ -26,11 +26,11 @@ function clicou() {
     else
         menuMobile.classList.add('active');
 
-        if(menuMobile.classList.contains('active'))
-            botao.classList.replace('fa-bars', 'fa-xmark');
+    if(menuMobile.classList.contains('active'))
+        botao.classList.replace('fa-bars', 'fa-xmark');
 
-        else
-            botao.classList.replace('fa-xmark', 'fa-bars');
+    else
+        botao.classList.replace('fa-xmark', 'fa-bars');
 }
 
 function clicouLink(){
@@ -41,15 +41,13 @@ function clicouLink(){
     else
         menuMobile.classList.add('active');   
 
-        if(menuMobile.classList.contains('active'))
-            botao.classList.replace('fa-bars', 'fa-xmark');
+    if(menuMobile.classList.contains('active'))
+        botao.classList.replace('fa-bars', 'fa-xmark');
 
-        else
-            botao.classList.replace('fa-xmark', 'fa-bars');
+    else
+        botao.classList.replace('fa-xmark', 'fa-bars');
 }
-
-
-form.addEventListener('submit', function enviouMensagem(evento) {
+ form.addEventListener('submit', function enviouMensagem(evento) {
     evento.preventDefault();
 
     alert('Mensagem enviada com sucesso!');
@@ -92,5 +90,47 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.classList.add('tema-escuro');
     } 
 });
+
+const carouselOptions = {
+    index: 0,
+    width: 0,
+    elmN: 0
+}
+const initCarousel = () => {
+    const carousel = document.getElementById('carousel');
+    const carouselWidth = carousel.offsetWidth;
+    carouselOptions.width = carouselWidth;
+    const carouselItens = document.getElementsByClassName('carousel-item');
+    carouselOptions.elmN = carouselItens.length;
+    for(let index in carouselItens){
+        const carouselItem = carouselItens[index];
+        if(!carouselItem.style) continue;
+        carouselItem.style.left = (index * carouselWidth) + 'px';
+    }
+}
+const carouselMove = (direction) => {
+    const carousel = document.getElementById('carousel');
+    const carouselItens = document.getElementsByClassName('carousel-item');
+    let move = carouselOptions.width * direction;
+    carouselOptions.index += direction;
+    if(carouselOptions.index >= carouselOptions.elmN){
+        carouselOptions.index = 0;
+        return initCarousel();
+    }else if (carouselOptions.index < 0){
+        carouselOptions.index = carouselOptions.elmN-1;
+        move *= carouselOptions.index * -1;
+    }
+    for(let index in carouselItens){
+        const carouselItem = carouselItens[index];
+        if(!carouselItem.style) continue;
+        const currentLeft = Number(carouselItem.style.left.replace('px', ''));
+        carouselItem.style.left = (currentLeft - move) + 'px';
+    }
+}
+window.onload = () => {
+    initCarousel()
+    document.getElementById('previous').onclick = () => {carouselMove(-1)};
+    document.getElementById('next').onclick = () => {carouselMove(1)};
+}
 
 
